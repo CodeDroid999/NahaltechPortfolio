7c4cadd9c647e4f90bd04061f4c775ef4822d0d2
CodeDroid99 <johnmwega2002@gmail.com>
2024-01-26 17:11:17 +0300

    fix meta

--- components/meta.tsx
+++ components/meta.tsx
@@ -38,7 +38,7 @@ const Meta = () => {
       <meta name="keywords" content="" />
       <meta name="description" content="" />
       <meta name="author" content="" />
-      <title>NAHAL TECHNOLOGIES | Telling our African stories through a camera lens</title>
+      <title>NAHAL TECHNOLOGIES | Harnessing the Power of Technology for a Better Future</title>
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
