5678eb5ca355c4bec2879821bd449fc56945741e
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-01 02:58:11 +0300

    add datacenet bg

--- pages/services/datacenter-design-and-architecture.tsx
+++ pages/services/datacenter-design-and-architecture.tsx
@@ -9,7 +9,8 @@ export default function DataCenters() {
     return (
         <Layout>
             <HeroSection Title1="Datacenter" Title2=" Design & Architecture" />
-            <ServiceSection p1="At Nahal Technologies, our datacenter design consulting practice offers end-to-end advisory spanning capacity planning, security protocols, power management, and cooling systems tailored to actualize the full potential of your IT infrastructure. With extensive experience building highly scalable, resilient and future-ready datacenters for enterprises across industries, our architects and engineers analyze current and projected workloads to determine optimal storage, computing capability, connectivity needs and redundancy measures vital for driving workflows and guarding against risks of downtime."
+            <ServiceSection bg="https://i.postimg.cc/LXYYQYts/data-center.jpg"
+                p1="At Nahal Technologies, our datacenter design consulting practice offers end-to-end advisory spanning capacity planning, security protocols, power management, and cooling systems tailored to actualize the full potential of your IT infrastructure. With extensive experience building highly scalable, resilient and future-ready datacenters for enterprises across industries, our architects and engineers analyze current and projected workloads to determine optimal storage, computing capability, connectivity needs and redundancy measures vital for driving workflows and guarding against risks of downtime."
                 p2=" By leveraging best practices honed through established partnerships with leading hardware and software vendors, we help craft truly tailored datacenters aligned to the unique goals of your organization." />
             <section id="services" className="features_section bg-gray-100 layout_padding2">
                 <div className="container">
