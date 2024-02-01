0549da6cc05f4b04900a2117f5945d3f7fc34360
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-01 03:42:15 +0300

    garra get the money

---
+++ components/services/ServiceOverviewL1.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+
+interface FeatureBoxProps {
+    p1: string;
+    p2: string;
+    bg: string;
+    Title: string;
+}
+
+const ServiceOverviewL1 = ({ p1, p2, bg, Title }) => {
+    return (
+        <section id="about" className="about_section layout_padding">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 px-0">
+                        <div className="img_container">
+                            <div className="img-box">
+                                <img src={bg} alt="bg" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 px-0">
+                        <div className="detail-box">
+                            <div className="heading_container">
+                                <h2>{Title}</h2>
+                            </div>
+                            <p>                          {p1}</p>
+                            <p>{p2}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ServiceOverviewL1;
--- pages/services.tsx
+++ pages/services.tsx
@@ -1,22 +1,21 @@
-import Head from 'next/head'
 
-import AboutSection from '../components/home/about-section'
+import HeroSection from 'components/services/HeroSection'
 import HeroArea from '../components/home/hero-section'
-import MissionSection from '../components/home/misssion-section'
-import ProjectsSection from '../components/home/projects-section'
-import ReviewsSection from '../components/home/reviews-section'
-import ServiceSection from '../components/home/service-section'
-import VisionSection from '../components/home/vission-section'
 import Layout from '../components/layout'
 import ContactForm from '../components/layout/contact-form'
+import ServiceOverviewL1 from 'components/services/ServiceOverviewl1'
+import ServiceOverviewL2 from 'components/services/ServiceOverviewL2'
 
 export default function Services() {
   return (
-      <Layout>
-        <HeroArea />
-        <ServiceSection />
-        <ReviewsSection/>
-        <ContactForm />
-      </Layout>
+    <Layout>
+      <HeroSection Title1="Our" Title2=" Services" />
+      <ServiceOverviewL1 Title="Software Development" bg="https://i.postimg.cc/25N5wcHV/AI.png" p1="At Nahal Technologies, our expert software engineers and developers specialize in crafting high-performance enterprise-grade software tailored to address our clients’ unique digital transformation needs. Whether building feature-rich web platforms, intuitive mobile applications or mission-critical cloud-based solutions, we pride ourselves on our ability to translate business requirements into eloquent code that achieves outcomes."
+        p2="Our development methodology focuses on rapid prototyping coupled with robust architecture optimized for security, scalability and maintainability across product lifecycles. If leveraging software to connect processes, unlock insights or achieve competitive advantage is your goal, our passionate team would welcome the opportunity to collaborate on delivering excellence." />
+      <ServiceOverviewL2 Title="Data Analytics" bg="https://i.postimg.cc/sgTrBTtk/data.png"
+        p1="At Nahal Technologies, Our data analytics solutions empower organizations to derive strategic, actionable insights using advanced analytics techniques including predictive modeling, machine learning algorithms, IoT data processing and custom BI dashboards."
+        p2="By leveraging real-time data feeds integrated from across business units, we provide visibility into micro trends while our data scientists uncover macro opportunities. If leveraging data intelligence to steer competitive advantage and ROI appeals to you, let's connect." />
+      <ContactForm />
+    </Layout>
   )
 }
